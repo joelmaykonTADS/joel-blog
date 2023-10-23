ae31ed912b69a7d3f6e70b5a78956d4c8dbe170f
joelmaykon <joelmaykon94@gmail.com>
2023-10-23 15:07:05 -0300

    feat: init project docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://joelmaykonblog.netlify.app/',
+  url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
